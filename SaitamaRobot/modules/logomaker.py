e5752a392d24bc2d09af4d7069198badc1463ff7
Lovely Boy <90893568+Hodacka@users.noreply.github.com>
2021-11-29 16:35:12 +0530

    Error Solved by @hodacka

--- SaitamaRobot/modules/logomaker.py
+++ SaitamaRobot/modules/logomaker.py
@@ -72,7 +72,7 @@ async def lego(event):
     pointsize = 500
     fillcolor = "black"
     shadowcolor = "blue"
-    fnt = glob.glob(".EmiliaAnimeBot/SaitamaRobot/resources/Vampire Wars.ttf/*")
+    fnt = glob.glob(".EmiliaAnimeBot/SaitamaRobot/resources/LOGO_FONT/*")
     randf = random.choice(fnt)
     font = ImageFont.truetype(randf, 140)
     w, h = draw.textsize(text, font=font)
